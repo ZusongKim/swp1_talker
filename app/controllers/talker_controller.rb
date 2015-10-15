47be4582d2db8a1ee5908a86f4a50d63e2b420b2
kimzusong <challengekzs@gmail.com>
2015-10-15 10:25:47 +0000

    talker created

--- app/controllers/talker_controller.rb
+++ app/controllers/talker_controller.rb
@@ -1,4 +1,18 @@
 class TalkerController < ApplicationController
   def index
+    @chats = Chat.all
   end
+  
+  def create
+    talk = Talk.where('ask LIKE ?', "%#{params[:ask]}%").sample
+    
+    unless talk.nil?
+      Chat.create(chat_type:"user", chat_content: params[:ask])
+      Chat.create(chat_type:"bot", chat_content: talk.answer)
+      redirect_to '/'
+    else
+      redirect_to '/learn'
+    end
+  end
+      
 end
